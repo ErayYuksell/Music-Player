a92e90f24fb45815ad0c562ea5c37d6ab205ca1e
Eray Yüksel <yukseleray203@gmail.com>
2022-07-21 09:56:15 +0300

    Display Music

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
+const container=document.querySelector(".container");
+const image=document.querySelector("#music-image");
+const title=document.querySelector(".title");
+const singer=document.querySelector(".singer");
+const prev=document.querySelector("#prev");
+const play=document.querySelector("#play");
+const next=document.querySelector("#next");
+
+
 const player = new MusicPlayer(musicList);
+window.addEventListener('load',() => {
+    let music=player.getMusic();
+    displayMusic(music);
+})
+ 
+function displayMusic(music){
+    title.innerText=music.title;
+    singer.innerText=music.singer;
+    image.src="img/" +music.img;
+    audio.src="mp3/" +music.file;
+}
 
- let music=player.getMusic();
-console.log(music.getName());
+play.addEventListener('click',() => {
+    audio.play();
+})
 
 
-player.next();
- music=player.getMusic();
-console.log(music.getName());
-
-player.previous();
- music=player.getMusic();
-console.log(music.getName());
