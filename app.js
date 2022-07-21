0ccc653a306fcb682ef56a020d545098b0cfbcf5
Eray Yüksel <yukseleray203@gmail.com>
2022-07-21 11:19:14 +0300

    Sure Kontrolu Tamamlandı

--- app.js
+++ app.js
@@ -36,26 +36,26 @@ prev.addEventListener('click',() => {
 next.addEventListener('click',() => {
     nextMusic();
 })
- function prevMusic(){
+ const prevMusic=() =>{
  player.prev();
  let music=player.getMusic();
  displayMusic(music);
  playMusic();
  }
- function nextMusic(){
+ const nextMusic=() =>{
  player.next();
  let music=player.getMusic();
  displayMusic(music);
  playMusic();
  }
 
-function pauseMusic(){
+ const pauseMusic=() =>{
     container.classList.remove("playing");
     play.classList=("fa-solid fa-play");
     audio.pause();
 }
 
-function playMusic(){
+const playMusic=() =>{
     container.classList.add("playing");
     play.classList=("fa-solid fa-pause");
     audio.play();
@@ -76,4 +76,7 @@ audio.addEventListener("timeupdate", () => {
     progressBar.value = Math.floor(audio.currentTime);
     currentTime.textContent = calculateTime(progressBar.value);
 });
-
+progressBar.addEventListener("input",() => {
+    currentTime.textContent=calculateTime(progressBar.value);
+    audio.currentTime=progressBar.value;
+})
